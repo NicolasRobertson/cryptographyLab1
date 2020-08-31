853c3d91732207b0c120432d4c6ed7f498d4e72e
Nic <robe1606@morris.umn.edu>
2020-08-30 22:17:14 -0500

    Add decryption algorithm

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,12 +1,13 @@
-
-
 public class VigenereCipher{
     public static void main(String args0[]){
-        String testString = makeValid("INVALID TEXT!!!");
-        String testKey = "loud";
+        String testString = "Sonic";
+        String testKey = "bluebird";
+        testString = makeValid(testString);
         System.out.println(testString);
         String encryptTestString = encrypt(testString, testKey);
         System.out.println(encryptTestString);
+        String decryptTestString = decrypt(encryptTestString, testKey);
+        System.out.println(decryptTestString);
     }
 
     public static String encrypt(String message, String key){
@@ -17,18 +18,33 @@ public class VigenereCipher{
             int messageValue = (int) character - 97;
             character = key.charAt(i%key.length());
             int keyValue = ((int) character - 97);
-            System.out.println(keyValue);
             int encryptedCharValue = ((messageValue + keyValue)%26) + 97;
 
             encryptedMessage += Character.toString((char)encryptedCharValue);
         }
+
         return encryptedMessage;
+    }
+
+    public static String decrypt(String message, String key){
+        String decryptedMessage = "";
+
+        for (int i=0; i<message.length(); i++){
+            char character = message.charAt(i);
+            int messageValue = (int) character - 19;
+            character = key.charAt(i%key.length());
+            int keyValue = ((int) character - 97);
+            int decryptedCharValue = ((messageValue - keyValue)%26) + 97;
+
+            decryptedMessage += Character.toString((char)decryptedCharValue);
+        }
+
+        return decryptedMessage;
     }
     
     private static char loopKey(String key,int index){
         int keyIndex = index % key.length();
-        return key.charAt(keyIndex);
-        
+        return key.charAt(keyIndex);        
     }
 
     public static String makeValid(String inValidText){
